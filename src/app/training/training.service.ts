e6463e4996e0b70f175b0dd31ab8740a8104d113
mac <mac@macs-MBP.lan>
2019-04-19 14:21:41 +0300

    pc

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -2,10 +2,14 @@ import { Exercise } from './exercise.module';
 
 
 export class TrainingService {
-    availableExercises: Exercise[] = [
+    private availableExercises: Exercise[] = [
         { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
         { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
         { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
         { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
     ]
+
+    getAvailableExercises(): any {
+        return this.availableExercises.slice()
+    }
 }
